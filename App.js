43e714b43b14effa752f75c3df4037280d022b08
Ashequl Haque <ashequlhaque009@gmail.com>
2021-12-15 20:16:17 -0500

    Initial setup of biteshare app

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from "styled-components/native";
+import { theme } from './src/infrastructure/index.js';
+import DummyComponent from './src/features/Dummy.js';
+
+export default function App() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+          <DummyComponent />
+      </ThemeProvider>
+
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
