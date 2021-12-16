0ec0ce820e41031acfb8afc206a05f1e3561f0d0
Ashequl Haque <ashequlhaque009@gmail.com>
2021-12-15 23:46:46 -0500

    Add hackreactor linter and remove airbnb linter

--- src/features/Dummy.js
+++ src/features/Dummy.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 const Title1 = styled.Text`
 color: ${(props) => props.theme.colors.brand.rausch}
-`
+`;
 const Title2 = styled.Text`
   color: ${(props) => props.theme.colors.brand.beach}
-`
+`;
 
 const DummyComponent = () => {
   return (
