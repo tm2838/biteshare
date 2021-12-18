5a9c013a62af56d9f2b4daaa176e7e77ba20060b
tingjunman <tm2838@nyu.edu>
2021-12-17 18:12:47 -0600

    fix linting

--- src/BiteShareContext.js
+++ src/BiteShareContext.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 export const biteShareState = {
   testState: 'This is a test state',
-}
+};
 
 export const biteShareReducer = (state, action) => {
   switch (action.type) {
-    case 'SET_TESTSTATE':
-      return {...state, testState: action.testState } // dispatch({ type: 'SET_TESTSTATE', testState: 'This is test No.2' })
-    default:
-      return state;
+  case 'SET_TESTSTATE':
+    return {...state, testState: action.testState }; // dispatch({ type: 'SET_TESTSTATE', testState: 'This is test No.2' })
+  default:
+    return state;
   }
-}
+};
 
 export const BiteShareContext = React.createContext({ state: biteShareState, dispatch: () => {} });
