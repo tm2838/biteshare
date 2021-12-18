6464418e29c2f0ddf665d3344585a40562ead2bb
tingjunman <tm2838@nyu.edu>
2021-12-17 18:10:32 -0600

    created context

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrastructure/index.js';
 import DummyComponent from './src/features/Dummy.js';
+import { BiteShareContext, biteShareReducer, biteShareState } from './src/BiteShareContext';
 
 export default function App() {
+  const [state, dispatch] = useReducer(biteShareReducer, biteShareState);
   return (
-    <>
+    <BiteShareContext.Provider value={{ state, dispatch }}>
       <ThemeProvider theme={theme}>
         <DummyComponent />
       </ThemeProvider>
 
-    </>
+    </BiteShareContext.Provider>
   );
 }
 
