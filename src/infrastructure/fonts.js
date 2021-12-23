ee3956d522b07f38cf7303b3782234503705ae58
Crystal Song <macbook_song@Crystals-MacBook-Pro.local>
2021-12-23 15:59:38 -0800

    remove font sizes

--- src/infrastructure/fonts.js
+++ src/infrastructure/fonts.js
@@ -13,15 +13,3 @@ export const fontWeights = {
   medium: 500,
   bold: 700,
 };
-
-export const fontSizes = {
-  caption: '12px',
-  button: '14px',
-  body: '16px',
-  title: '20px',
-  h5: '24px',
-  h4: '34px',
-  h3: '45px',
-  h2: '56px',
-  h1: '112px',
-};
