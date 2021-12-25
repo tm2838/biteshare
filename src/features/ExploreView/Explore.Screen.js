0967cf483e387b79426e9efa6238591d1a10ef80
Christina Nathan <christina.a.nate@gmail.com>
2021-12-25 15:14:52 -0800

    implemented ui for join tab

--- src/features/ExploreView/Explore.Screen.js
+++ src/features/ExploreView/Explore.Screen.js
@@ -2,12 +2,30 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import ExploreHeader from './ExploreHeader';
 import SafeArea from '../../components/SafeArea';
+import { colors } from '../../infrastructure/colors';
+
+
+const styles = StyleSheet.create({
+  exploreTitle: {
+    color: colors.brand.kazan,
+    fontSize: 24,
+  },
+
+  exploreTitleContainer: {
+    // flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+});
 
 const ExploreScreen = () => {
   return (
     <SafeArea>
       <View>
         <ExploreHeader />
+        <View style={styles.exploreTitleContainer}>
+          <Text style={styles.exploreTitle}>Testing</Text>
+        </View>
       </View>
     </SafeArea>
   );
