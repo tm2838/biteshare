6afc92a4d93d5861fa2a44f997b2b6e8dd4f861d
Jeremiah <JeremiahBerndt@gmail.com>
2021-12-26 19:31:42 -0700

    most of UI for login page complete

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import { TextInput, StyleSheet, View } from 'react-native';
+import { theme } from '../infrastructure/index';
 
 const styles = StyleSheet.create({
   input: {
-    padding: 10
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  container: {
+    padding: 5
   }
 });
 
-export default function InputField({ inputValue, placeholder, secureText }) {
-  // const [inputValue, setInputValue] = useState(null)
+export default function InputField({ placeholder, secureText }) {
+  const [inputValue, setInputValue] = useState(null);
 
-  return (<TextInput
-    // value={value}
-    onChangeText={inputValue}
-    placeholder={placeholder}
-    style={styles.input}
-    secureTextEntry={secureText}
-  />);
+  return (
+    <View style={styles.container}>
+      <TextInput
+        elevation={5}
+        value={inputValue}
+        onChangeText={setInputValue}
+        placeholder={placeholder}
+        style={styles.input}
+        secureTextEntry={secureText}
+      />
+    </View>
+  );
 }
 
 
--- src/features/LoginView/Login.Screen.js
+++ src/features/LoginView/Login.Screen.js
@@ -7,29 +7,49 @@ import { theme } from '../../infrastructure/index';
 // console.log('colors', theme);
 const styles = StyleSheet.create({
   loginContainer: {
+    flex: 1,
     backgroundColor: theme.colors.brand.login,
-    flex: 1
   },
-  title: {
-    flex: 2
+  biteshareTitle: {
+    flex: 1,
+    textAlign: 'center',
+    fontFamily: theme.fonts.heading,
+    fontSize: 64,
+    marginTop: 80
+  },
+  loginTitle: {
+    flex: 1,
+    textAlign: 'center',
+    fontFamily: theme.fonts.heading,
+    fontSize: 32,
   },
   loginEntries: {
-    flex: 1
+    flex: 3,
+    padding: 20
+  },
+  signUp: {
+    flex: 1,
+    textAlign: 'center',
+    fontFamily: theme.fonts.heading,
+    textDecorationLine: 'underline'
   }
 });
-
 
 export default function LoginScreen() {
   return (
     <SafeArea>
       <View style={styles.loginContainer}>
-        <Text style={styles.title}>BiteShare</Text>
-        <InputField
-          placeholder={'Username'}
-          secureText={false} />
-        <InputField
-          placeholder={'Password'}
-          secureText={true} />
+        <Text style={styles.biteshareTitle}>BITESHARE</Text>
+        <Text style={styles.loginTitle}>Login</Text>
+        <View style={styles.loginEntries}>
+          <InputField
+            placeholder={'Username'}
+            secureText={false} />
+          <InputField
+            placeholder={'Password'}
+            secureText={true} />
+          <Text>Don't have an account? <Text style={styles.signUp}>Sign Up</Text></Text>
+        </View>
       </View>
     </SafeArea>
   );
