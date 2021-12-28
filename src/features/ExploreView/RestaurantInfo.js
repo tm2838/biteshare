1701a2cb617b90b3cf9532c69d99309cde0e86dd
Christina Nathan <christina.a.nate@gmail.com>
2021-12-27 20:30:43 -0800

    when pressing show full menu, alert window pops up with restaurantId

--- src/features/ExploreView/RestaurantInfo.js
+++ src/features/ExploreView/RestaurantInfo.js
@@ -28,38 +28,31 @@ const RestaurantInfo = ({ restaurant }) => {
   //   cuisines = [],
   //   address = { street: '111 Made up Street' }
   // } = restaurant;
+  const seeFullMenuButtonPress = (restaurantId) => {
+    alert(`restaurantId: ${restaurantId}`);
+  };
 
   console.log('RESTAURANT: ', restaurant);
   return (
     <View style={styles.restaurantContainer}>
-      {/* <Card style={styles.card} elevation={2}>
-        <Card.Cover key={restaurant_name} source={{ uri: 'https://picsum.photos/700' }} />
-        <Card.Title title={restaurant_name} subtitle={price_range} />
+      <Card style={styles.card} elevation={2}>
+        <Card.Cover key={restaurant.restaurant_name} source={{ uri: 'https://picsum.photos/700' }} />
+        <Card.Title title={restaurant.restaurant_name} subtitle={restaurant.price_range || 'price'} />
         <Card.Content>
-          <Text>{cuisines[0] || '----'}</Text>
-          <Text>{restaurant_phone}</Text>
-          <Text>{address.street}</Text>
+          <Text>{restaurant.cuisines[0] || 'cuisine'}</Text>
+          <Text>{restaurant.restaurant_phone}</Text>
+          <Text>{restaurant.address.street}</Text>
+          {/* <Text>{restaurant.restaurant_id}</Text> */}
         </Card.Content>
         <Card.Actions>
           <Button
-            onPress={() => alert('clicked!')}
+            onPress={() => seeFullMenuButtonPress(restaurant.restaurant_id)}
             color={colors.brand.body}
             mode={'contained'}>
             See Full Menu
           </Button>
         </Card.Actions>
-      </Card> */}
-      <Text>{restaurant.address.street}</Text>
-      <Text>{restaurant.restaurant_name}</Text>
-      <Text>{restaurant.restaurant_phone}</Text>
-      <Text>{restaurant.price_range || 'price'}</Text>
-      <Text>{restaurant.cuisines[0] || 'cuisine'}</Text>
-      <Text>{restaurant.restaurant_id}</Text>
-
-
-
-
-
+      </Card>
     </View>
   );
 };
