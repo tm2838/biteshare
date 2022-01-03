964c805cdf852e9bec7347838b266d1ae884c1ee
Josh Kohl <joshdkohl@gmail.com>
2022-01-03 17:12:33 -0500

    Created Account Header Component. Back button directs to Profile page.

---
+++ src/features/ProfileView/AccountSettings/AccountScreenHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Appbar, Avatar } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { colors } from '../../../infrastructure/colors';
+
+
+const styles = StyleSheet.create({
+  profileScreenHeader: {
+    backgroundColor: colors.brand.kazan
+  }
+});
+
+const AccountScreenHeader = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  let previousScreen = route.name === 'Account' ? 'Profile' : '';
+  const handleBackButton = () => navigation.navigate(previousScreen);
+
+  return (
+    <Appbar.Header style={styles.profileScreenHeader}>
+      <Appbar.BackAction onPress={handleBackButton} color="white" />
+    </Appbar.Header>
+  );
+};
+
+export default AccountScreenHeader;
