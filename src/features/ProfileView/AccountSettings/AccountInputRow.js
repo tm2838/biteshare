cd8ac7f401eb617482b9433f6424077b5ff40c0a
Josh Kohl <joshdkohl@gmail.com>
2022-01-07 21:06:55 -0500

    Conditional rendering of text or form field plus icon on account info rows

--- src/features/ProfileView/AccountSettings/AccountInputRow.js
+++ src/features/ProfileView/AccountSettings/AccountInputRow.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
+import { Icon } from 'react-native-elements';
+
 import { colors } from '../../../infrastructure/colors';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import { BiteShareContext } from '../../../BiteShareContext';
 
 const styles = StyleSheet.create({
@@ -22,24 +24,53 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 15
   },
-  heading: {
-
-  },
-  info: {
-
-  },
-  input: {
+  icon: {
 
   }
 });
 
 const AccountInfo = ({ heading, info }) => {
 
+  //TODO
+  //Create state variable to conditionally render row
+  //Edit state from icon click
+
+  //TODO
+  //ON SUBMIT FX() TO UPDATE USER COLLECTION IN DATABASE
+  //CALL renderInfo() ON COMPLETTION TO REVERT BACK TO TEXT INPUT
+  //MAY NEED TO PLACE THIS FX() IN PARENT COMPONENT TO RERENDER WITH PROPER INFO
+
+
+  const renderInfo = (view) => {
+    if (view === 'form') {
+      return (
+        <TextInput
+          style={styles.text}
+          placeholder={info}
+          // onChangeText={update text field fx}
+          // onFormSubmit={}?
+        />
+      );
+    }
+
+    return (
+      <Text style={styles.text}>{info}</Text>
+    );
+
+  };
+
   return (
     <View style={styles.container}>
 
       <Text style={styles.text}>{heading}</Text>
-      <Text style={styles.text}>{info}</Text>
+      {renderInfo()}
+
+      <Icon
+        name='build'
+        size={20}
+        onClick={renderInfo('form')}
+      />
+
 
     </View>
   );
